f50c639d9f0c6f8e68e0f0af7a99dd91c8ea0aea
MSCRATCH <me@pathologicalplay.com>
2026-01-09 13:20:28 +0100

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+defined('MAIN') or die("Direct access to this file is restricted.");
+
+//This file is part of PHPUC
+//config.php
+//MMXXVI MSCRATCH
+
+define('SERVERNAME', '');
+define('USERNAME', '');
+define('PASSWORD', '');
+define('DB_NAME', '');
